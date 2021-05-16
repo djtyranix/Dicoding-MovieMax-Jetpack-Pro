758b3b00f40ab954a2d75321b2001fa68cd6319d
djtyranix <37214301+djtyranix@users.noreply.github.com>
2021-05-16 11:17:59 +0700

    Update home views, Added dark mode switch

--- app/src/main/java/com/nixstudio/moviemax/views/settings/SettingsFragment.kt
+++ app/src/main/java/com/nixstudio/moviemax/views/settings/SettingsFragment.kt
@@ -1,22 +1,73 @@
 package com.nixstudio.moviemax.views.settings
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import androidx.preference.SwitchPreference
 import com.nixstudio.moviemax.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     private var languageChange: Preference? = null
+    private var darkModeSwitch: SwitchPreference? = null
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val preference = PreferenceManager.getDefaultSharedPreferences(activity)
+        val editor = preference?.edit()
 
         languageChange = findPreference("select_language")
 
         val languageIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
         languageChange?.intent = languageIntent
+
+        darkModeSwitch = findPreference("enable_dark_mode")
+        val isDarkModeEnabled = preference.getBoolean("isDarkModeEnabled", false)
+
+        if (isDarkModeEnabled) {
+            AppCompatDelegate
+                .setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_YES
+                )
+        } else {
+            AppCompatDelegate
+                .setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_NO
+                )
+        }
+
+        darkModeSwitch?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { _, _ ->
+                if (darkModeSwitch?.isChecked == true) {
+                    AppCompatDelegate
+                        .setDefaultNightMode(
+                            AppCompatDelegate.MODE_NIGHT_NO
+                        )
+
+                    editor?.putBoolean("isDarkModeEnabled", false)
+                    editor?.apply()
+                    darkModeSwitch?.isChecked = false
+                    Toast.makeText(activity, resources.getString(R.string.dark_mode_isenabled, resources.getString(R.string.disabled)), Toast.LENGTH_SHORT).show()
+                } else {
+                    AppCompatDelegate
+                        .setDefaultNightMode(
+                            AppCompatDelegate.MODE_NIGHT_YES
+                        )
+
+                    editor?.putBoolean("isDarkModeEnabled", true)
+                    editor?.apply()
+                    darkModeSwitch?.isChecked = true
+                    Toast.makeText(activity, resources.getString(R.string.dark_mode_isenabled, resources.getString(R.string.enabled)), Toast.LENGTH_SHORT).show()
+                }
+
+                false
+            }
     }
 }
--- app/src/main/java/com/nixstudio/moviemax/views/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/nixstudio/moviemax/views/splashscreen/SplashScreenActivity.kt
@@ -1,11 +1,14 @@
 package com.nixstudio.moviemax.views.splashscreen
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
 import com.nixstudio.moviemax.R
 import com.nixstudio.moviemax.views.home.HomeActivity
 
@@ -15,6 +18,21 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         window.statusBarColor = ContextCompat.getColor(this, R.color.lightblue_500)
+
+        val preference = PreferenceManager.getDefaultSharedPreferences(this)
+        val isDarkModeEnabled = preference.getBoolean("isDarkModeEnabled", false)
+
+        if (isDarkModeEnabled) {
+            AppCompatDelegate
+                .setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_YES
+                )
+        } else {
+            AppCompatDelegate
+                .setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_NO
+                )
+        }
 
         Handler(Looper.getMainLooper()).postDelayed({
             val mainIntent = Intent(this, HomeActivity::class.java)
